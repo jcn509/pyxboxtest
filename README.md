ab738099d8619ba5d35cf36655846b642c3732aa
Josh Neil <joshneil8@googlemail.com>
2020-10-07 15:04:08 +0100

    basic refactoring

--- README.md
+++ README.md
@@ -2,7 +2,35 @@
 Supports XQEMU and hopefully soon CXBX-R. XQEMU may well be better, as it is unclear how well supported nxdk is with CXBX-R. CXBX-R support would be nice as with CXBX-R (unlike with XQEMU) you do not need a copy of the BiOS or MCPX rom, and that would make it far easier to run tests on CI platforms legally.
 
 # Implemented
- - Running games
+ - Running games/apps
  - Capturing kernel debug output
+ - Controller input (uses Qemu's monitor rather than faking keyboard events so you can use your computer for other tasks without it interfering)
  - Screenshots
+ - Headless mode so that you can run tests in the background without windows popping up
 
+# TODO
+## Definitely
+ - Support for HDD read/write (outside of Xbox apps)
+   - Until there is a nice way to read/write HDD images on PC it will require either:
+     - An app with FTP or some other protocol implemented running on XQEMU
+       - Would be nice to provide a library that can be linked againts to allow this while your app is running
+       
+         - This may take the form of some extension to 
+     - Some legal/opensource alternative to XBDM.dll
+     - Some wrapper around fatxplorer or some other tool might be possible?
+ - Documentation
+
+## If time allows
+ - Support for automated HDD copy/cleanup
+   - Automatically copy a HDD image before an app is run so that the changes made won't affect other tests
+ - Investigate https://github.com/mborgerson/xqemu-kernel. I couldn't get it to work :/ but if it could be used it would provide a way to run tests legally on some CI platform (currently tricky due to legal issues with the kernel)
+ - Support for CXBX-R (tests should work regardless of which emulator is used, without much modification)
+   - Currently NXDK support is not very good, so this may not happen for some time
+   - May not be possible to support:
+     - headless mode
+     - sending fake inputs to the app without generating fake keyboard input that would potentially interfere with other applications
+
+## Once everthing else is done
+  - Investigate support for qemu monitor based debugging
+   - [xboxpy](https://github.com/XboxDev/xboxpy) will work __today__ so long as it works with XQEMU. We may be able to do some additional debugging using XQEMU's monitor without additional overhead
+ - Investigate support for running tests on real hardware. Should be possible using (some open source alternative to) XBDM.dll to control the box.
--- kernel_debug.py
+++ kernel_debug.py
@@ -1,108 +1,143 @@
 # -*- coding: utf-8 -*-
+from contextlib import AbstractContextManager
 import socket
-import os
-
 import subprocess
 import time
 
-from typing import Optional
+from typing import Callable, Optional, Union
 
 import qmp
 
-KD_SOCKET = "/tmp/xserial"
 
-def _connect_to_sock(socket_name: str, max_retries: int = 100, retry_delay: int = 100):
-    client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    
-    attempts = 0
-    max_attempts = 100
-    while attempts < max_attempts:
+def _retry_every(
+    thing_to_try: Callable[[], None], max_retries: int = 100, retry_delay: int = 100
+):
+    num_tries = 0
+    while num_tries < max_retries:
         try:
-            client.connect(socket_name)
+            thing_to_try()
         except:
-            attempts += 1
+            num_tries += 1
             time.sleep(1)
             pass
         else:
-            print("connected", socket_name)
-            return client
+            return
 
-    if attempts == max_attempts:
-        raise Exception("fail lol")
-
+    if num_tries == max_retries:
+        raise RuntimeError("Attempt failed")
 
 
+class XQEMUKDCapturer:
+    def __init__(self, socket_addres):
+        self._client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        _retry_every(lambda: self._client.connect(socket_addres))
 
+    def close(self) -> None:
+        self._client.close()
 
-class KDCapture:
-    def __init__(self):
-        self._client = _connect_to_sock(KD_SOCKET)
-        
-    def get_line(self, max_size = 1, delim="\n"):
-        char = self._client.recv(max_size).decode('ascii')
-        data = char 
-        while char != delim:
-            char = self._client.recv(max_size).decode('ascii')
-            data+=char
+    def get_num_chars(self, num_chars: int, encoding: str = "ascii") -> str:
+        return self._client.recv(num_chars).decode(encoding)
+
+    def get_line(self, delim_char="\n", encoding: str = "ascii") -> str:
+
+        # Go character by character so that we don't read too far ahead.
+        # However, this method may be slow so the implementation may change in the future...
+        chunk_size = 1
+        char = self._client.recv(chunk_size).decode(encoding)
+        data = char
+        while char != delim_char:
+            char = self._client.recv(chunk_size).decode(encoding)
+            data += char
         return data
-    # Do  need to disconnect in destructor or is that done for me anyway?
 
 
+KD_SOCKET = "/tmp/xserial"
 MONITOR_SOCKET = "/tmp/xqemu-monitor-socket"
-class XQEMUMonitor:
-    def __init__(self):
-        self._client = qmp.QEMUMonitorProtocol(MONITOR_SOCKET)
-        
-        attempts = 0
-        max_attempts = 10
-        while attempts < max_attempts:
-            try:
-                self._client.connect()
-            except:
-                attempts += 1
-                time.sleep(1)
-                print("faile")
-            else:
-                print("connected", MONITOR_SOCKET)
-                break
 
-    def press_key(self, key: str, hold_time: Optional[int] = None):
+
+class XboxApp(AbstractContextManager):
+    def __init__(
+        self,
+        hdd_filename: Optional[str] = None,
+        dvd_filename: Optional[str] = None,
+        headless: bool = False,
+    ):
+        mcpx_rom = "/home/josh/.xqemu_files/mcpx_1.0.bin"
+        xqemu_binary = "/bin/xqemu"
+        xbox_bios = "/home/josh/.xqemu_files/Complex_4627.bin"
+
+        self._xqemu_args = (
+            xqemu_binary,
+            "-cpu",
+            "pentium3",
+            "-machine",
+            f"xbox,bootrom={mcpx_rom},short_animation",
+            "-m",
+            "64M",
+            "-bios",
+            xbox_bios,
+            "-device",
+            "usb-xbox-gamepad",
+            "-device",
+            "lpc47m157",
+            "-serial",
+            "unix:" + KD_SOCKET + ",server,nowait",
+            "-qmp",
+            "unix:" + MONITOR_SOCKET + ",server,nowait",
+        )
+        if headless:
+            self._xqemu_args += ("-display", "egl-headless")
+
+        if hdd_filename is not None:
+            self._xqemu_args += ("-drive", f"file={hdd_filename},index=0,media=disk")
+        if dvd_filename is not None:
+            self._xqemu_args += ("-drive", f"index=1,media=cdrom,file={dvd_filename}")
+
+    def press_key(self, key: str, hold_time: Optional[int] = None) -> None:
         # Need to figure out how to set hold time :s
-        args = {
-            "keys": [
-                { "type": "qcode", "data": key }
-            ]
-        }
+        args = {"keys": [{"type": "qcode", "data": key}]}
         if hold_time is not None:
             args["hold-time"] = hold_time
-        self._client.command("send-key", **args)
+        self._qemu_monitor.command("send-key", **args)
 
-    def save_screenshot(self, filename: str):
-        self._client.command("screendump", filename = filename, )
+    def save_screenshot(self, filename: str) -> None:
+        self._qemu_monitor.command("screendump", filename=filename)
 
-def run_app():
-    # May want to force waiting for KD/Monitor, but doing so is annoying if using both...
-    # could perhaps just force wait on one of them?
-    return subprocess.Popen(["/bin/xqemu", "-cpu", "pentium3", "-machine", "xbox,bootrom=/home/josh/.xqemu_files/mcpx_1.0.bin,short_animation", "-m", "64M", "-bios", "/home/josh/.xqemu_files/Complex_4627.bin", "-drive", "file=/home/josh/.xqemu_files/xbox_hdd.qcow2,index=0,media=disk", "-drive", "index=1,media=cdrom,file=/home/josh/projects/ind_revived_os/hello.iso", "-device", "usb-xbox-gamepad", "-device", "lpc47m157", "-serial", "unix:" + KD_SOCKET +",server,nowait", "-qmp", "unix:" + MONITOR_SOCKET + ",server,nowait", "-display", "egl-headless"],
-    stdin=subprocess.PIPE
-            )
+    def get_kd_capturer(self) -> XQEMUKDCapturer:
+        return self._kd_capturer
+
+    def __enter__(self):
+        self._app = subprocess.Popen(self._xqemu_args)
+        self._qemu_monitor = qmp.QEMUMonitorProtocol(MONITOR_SOCKET)
+        self._kd_capturer = XQEMUKDCapturer(KD_SOCKET)
+        _retry_every(lambda: self._qemu_monitor.connect())
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._qemu_monitor.close()
+        self._qemu_monitor = None
+        self._kd_capturer.close()
+        self._kd_capturer = None
+        self._app.kill()
+        self._app = None
+
 
 def test_usb_code():
-    app = run_app()
-    monitor = XQEMUMonitor()
-    kd = KDCapture()
-    print("gonna try for some output now")
+    with XboxApp(
+        hdd_filename="/home/josh/.xqemu_files/xbox_hdd.qcow2",
+        dvd_filename="/home/josh/projects/ind_revived_os/hello.iso",
+        headless=True,
+    ) as app:
+        print("gonna try for some output now")
+        kd = app.get_kd_capturer()
+        nothing_pressed = kd.get_line()
+        a_key_text = '"keys":"A"'
+        assert a_key_text not in nothing_pressed, "A not pressed"
 
-    nothing_pressed = kd.get_line()
-    a_key_text = '"keys":"A"'
-    assert a_key_text not in nothing_pressed, "A not pressed"
+        app.press_key("s", hold_time=50)
+        a_pressed = kd.get_line()
+        print(a_pressed, a_key_text in a_pressed)
+        assert a_key_text in a_pressed, "A not pressed"
 
-    monitor.press_key("s", hold_time=50)
-    a_pressed = kd.get_line()
-    print(a_pressed, a_key_text in a_pressed)
-    assert a_key_text in a_pressed, "A not pressed"
-
-    a_released = kd.get_line()
-    assert a_key_text not in a_released, "A not pressed"
-
-    app.kill()
+        a_released = kd.get_line()
+        assert a_key_text not in a_released, "A not pressed"
