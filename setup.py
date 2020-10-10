c46af60eceb6c8d85131f018613f30db741e79a5
Josh Neil <joshneil8@googlemail.com>
2020-10-10 12:42:03 +0100

    setup.py for library install - need to auto sync with pipfile

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="pyxboxtest",
+    version="0.1",
+    description="An XQEMU and pytest based end-to-end testing framework for original Xbox apps",
+    author="Josh Neil",
+    author_email="joshneil8@gmail.com",
+    # url='https://www.python.org/sigs/distutils-sig/',
+    install_requires=["pytest", "qmp"],
+    packages=["pyxboxtest"],
+)
