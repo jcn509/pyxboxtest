ed40a8227fa6faf0f0c4e8392d6f66fb43963e15
Josh Neil <joshneil8@googlemail.com>
2020-11-02 21:37:18 +0000

    Fix comments

--- tests/xqemu/hdd/test_xqemu_hdd_modifications.py
+++ tests/xqemu/hdd/test_xqemu_hdd_modifications.py
@@ -1,8 +1,16 @@
-"""Test for the subclasses of :py:class:`pyxboxtest.hdd.HDDMofication`"""
-from pyxboxtest.xqemu.hdd.xqemu_hdd_modifications import HDDModification
-from pyxboxtest.xqemu.hdd._xqemu_hdd_template_modifier import _XQEMUHDDTemplateModifier
+"""Test for the subclasses of :py:class:`pyxboxtest.xqemu.hdd.HDDModification`
+
+Ensures that they actually perform the operations that they are designed
+to perform (adding files, removing files, etc).
+
+Note: these tests only ensure that they make the correct calls to the classes
+that will actually perform the changes. They don't check the contents of any
+HDDs...
+"""
 import pytest
 
+from pyxboxtest.xqemu.hdd.xqemu_hdd_modifications import HDDModification
+from pyxboxtest.xqemu.hdd._xqemu_hdd_template_modifier import _XQEMUHDDTemplateModifier
 from pyxboxtest.xqemu.hdd import AddFile, BatchModification, DeleteFile, RenameFile
 
 
@@ -21,7 +29,7 @@ def mock_xqemu_hdd_template_modifier(mocker):
 def test_add_file(
     local_filename: str, xbox_filename: str, mock_xqemu_hdd_template_modifier
 ):
-    """Ensures that :py:class:`pyxboxtest.hdd.AddFile` actually adds the file
+    """Ensures that :py:class:`pyxboxtest.xqemu.hdd.AddFile` actually adds the file
     to the HDD
     """
     AddFile(local_filename, xbox_filename).perform_modification(
@@ -34,7 +42,7 @@ def test_add_file(
 
 @pytest.mark.parametrize("filename", ("file1", "file2"))
 def test_delete_file(filename: str, mock_xqemu_hdd_template_modifier):
-    """Ensures that :py:class:`pyxboxtest.hdd.DeleteFile` actually deletes the
+    """Ensures that :py:class:`pyxboxtest.xqemu.hdd.DeleteFile` actually deletes the
     file from the HDD
     """
     DeleteFile(filename).perform_modification(mock_xqemu_hdd_template_modifier)
@@ -45,7 +53,7 @@ def test_delete_file(filename: str, mock_xqemu_hdd_template_modifier):
 def test_rename_file(
     old_filename: str, new_filename: str, mock_xqemu_hdd_template_modifier
 ):
-    """Ensures that :py:class:`pyxboxtest.hdd.RenameFile` actually renames the
+    """Ensures that :py:class:`pyxboxtest.xqemu.hdd.RenameFile` actually renames the
     file on the HDD
     """
     RenameFile(old_filename, new_filename).perform_modification(
@@ -60,7 +68,7 @@ def test_rename_file(
 def test_batch_modification(
     num_modifications: int, mock_xqemu_hdd_template_modifier, mocker
 ):
-    """Ensures that :py:class:`pyxboxtest.hdd.BatchModifcation` actually
+    """Ensures that :py:class:`pyxboxtest.xqemu.hdd.BatchModifcation` actually
     performs the sequence of modifications that it was created for
     """
     modifications = tuple(
