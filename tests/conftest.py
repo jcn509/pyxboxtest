8338911da71aa04cb0a41f8cab212b00d002859a
Josh Neil <joshneil8@googlemail.com>
2020-11-08 16:06:03 +0000

    Test temporary directories

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,13 @@
 """Configuration for the tests"""
 # pylint: disable=unused-import
+import pytest
+
 from pyxboxtest.xqemu.hdd import xqemu_blank_hdd_template
 
 pytest_plugins = ("pyxboxtest.pytest_plugin",)
+
+
+@pytest.fixture
+def mocked_subprocess_popen(mocker):
+    """We don't really want to spawn new processes"""
+    return mocker.patch("subprocess.Popen")
