0f74d3e237d4ac93634dad1b1f308c323debce97
Josh Neil <joshneil8@googlemail.com>
2021-04-22 20:34:52 +0100

    fix broken tests

--- tests/end_to_end_tests/xqemu/hdd/test_xqemu_hdd_image_modifier.py
+++ tests/end_to_end_tests/xqemu/hdd/test_xqemu_hdd_image_modifier.py
@@ -90,26 +90,26 @@ def test_directory_creation(
 def test_delete_empty_directory(xqemu_blank_hdd_template: XQEMUHDDTemplate):
     hdd_image = xqemu_blank_hdd_template.create_fresh_hdd()
     with XQEMUHDDImageModifer(hdd_image) as modifier:
-        modifier.add_directory_to_xbox("/C/foo")
+        modifier.add_directory_to_xbox("/C/foo/")
  
         assert modifier.get_xbox_directory_contents("/C/") == ["foo"], "directory exists before"
 
         modifier.delete_directory_from_xbox("/C/foo/")
 
-        assert modifier.get_xbox_directory_contents("/C/") == [""], "directory was deleted"
+        assert modifier.get_xbox_directory_contents("/C/") == [], "directory was deleted"
 
 
 def test_delete_directory_with_content(xqemu_blank_hdd_template: XQEMUHDDTemplate):
     hdd_image = xqemu_blank_hdd_template.create_fresh_hdd()
     with XQEMUHDDImageModifer(hdd_image) as modifier:
-        modifier.add_directory_to_xbox("/C/dir")
+        modifier.add_directory_to_xbox("/E/dir/")
         modifier.add_file_to_xbox(
-            "/C/dir/file.txt", BytesIO(f"some content".encode("utf8"))
+            "/E/dir/file.txt", BytesIO(f"some content".encode("utf8"))
         )
-        assert modifier.get_xbox_directory_contents("/C/") == ["dir"], "directory exists before"
-        assert modifier.get_xbox_directory_contents("/C/dir/") == ["file.txt"], "file exists before"
+        assert modifier.get_xbox_directory_contents("/E/") == ["dir"], "directory exists before"
+        assert modifier.get_xbox_directory_contents("/E/dir/") == ["file.txt"], "file exists before"
 
-        modifier.delete_directory_from_xbox("/C/dir/")
+        modifier.delete_directory_from_xbox("/E/dir/")
 
-        assert modifier.get_xbox_directory_contents("/C/") == [""], "directory and file were both deleted"
+        assert modifier.get_xbox_directory_contents("/E/") == [], "directory and file were both deleted"
 
